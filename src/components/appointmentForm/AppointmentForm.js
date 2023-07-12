63ef0da6bd7257e90c178706221dcad798dd5d6f
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-07-12 13:52:44 +0200

    Porblem 9 solved (i guess)

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -25,12 +25,17 @@ export const AppointmentForm = ({
 		<>
 			<form onSubmit={handleSubmit}>
 				<label for="title">Title of the Appointment</label>
-				<input type="text" name="title" id="title" onChange={setTitle((e) => e.target.value)}/>
-				<ContactPicker contacts={contact}></ContactPicker>
+				<input type="text" name="title" id="title" onChange={setTitle((e) => e.target.value)} value={title}
+					   required/>
+				<label for="contact">Select contact</label>
+				<ContactPicker contacts={contacts} onChange={setContact((e) => e.target.value)}
+							   value={contact} name="contact"></ContactPicker>
 				<label for="date">Date</label>
-				<input type="date" name="date" id="date" onChange={setDate((e) => e.target.value)}/>
+				<input type="date" name="date" id="date" onChange={setDate((e) => e.target.value)} value={date}
+					   required/>
 				<label for="time">Time</label>
-				<input type="time" name="time" id="time" onChange={setTime((e) => e.target.value)}/>
+				<input type="time" name="time" id="time" onChange={setTime((e) => e.target.value)} value={time}
+					   required/>
 				<input type="submit" name="submit" id="submit" value="Submit"/>
 			</form>
 		</>
--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
-  return (
-    <></>
-  );
+export const ContactPicker = ({contacts, onChange, value, name}) => {
+	return (
+		<>
+			<select name={name} value={value} onChange={onChange}>
+				<option value="" key={"-1"}>No Contact selected</option>
+				{contacts.map((contact) => {
+					<option value={contact} key={contact}>{contact}</option>
+				})}
+			</select>
+		</>
+	);
 };
